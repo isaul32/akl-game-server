72f0ddb9ef27081f15102215fe3fbc9710170702
isaul32 <isaul32@users.noreply.github.com>
2017-07-05 23:26:27 +0300

    Update akl2.sh

--- instances-scripts/akl2.sh
+++ instances-scripts/akl2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/home/steam/csgo/srcds_run -autoupdate -game csgo -usercon -strictportbind -port 27115 -tickrate 128 -port 27015 +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2
+/home/steam/csgo/srcds_run -autoupdate -game csgo -usercon -ip 0.0.0.0 -port 27115 -tickrate 128 +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
