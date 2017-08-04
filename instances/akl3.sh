00ea813dbc64bf5d92a6a1b2a4c5e4c30b7cb787
Sauli Anto <sauli.anto@gofore.com>
2017-08-04 17:11:06 +0300

    Fix game updating

--- instances/akl3.sh
+++ instances/akl3.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+/home/steam/Steam/steamcmd.sh +login anonymous +force_install_dir /home/steam/csgo +app_update 740 +quit
 /home/steam/csgo/srcds_run -autoupdate -game csgo -usercon -strictportbind -ip xxx.xxx.xxx.xxx -port 27015 -tickrate 128 +clientport 27005 +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX +sv_minrate 128000 +sv_mincmdrate 128 +sv_minupdaterate 128 +host_players_show 2 +show_info_show 2 +log on +logaddress_add xxx.xxx.xxx.xxx:xxxx +hostname "AKL#3" +sv_hibernate_postgame_delay 120
